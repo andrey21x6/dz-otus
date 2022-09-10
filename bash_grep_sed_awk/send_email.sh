53ae0b072f98c7d6df4712613030e3bbac7cbeec
andrey21x6 <91377497+andrey21x6@users.noreply.github.com>
2022-09-10 15:48:39 +0500

    Update send_email.sh

--- bash_grep_sed_awk/send_email.sh
+++ bash_grep_sed_awk/send_email.sh
@@ -10,38 +10,38 @@ declare domainsListMax=""
 declare errorList=""
 declare httpList=""
 
-logTest=`cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }'`
+logTest=$(< log.log awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }')
 
 if [ -z "${logTest}" ]; then
-	cat log.log | grep -o -E "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > ip_list.txt
-	cat log.log | grep -o -P "[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > domains_list.txt
-	cat log.log | grep "error" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > error_list.txt
-	cat log.log | grep -o -E " HTTP/1.(0|1)\" [0-9]{3,3} " | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > http_list.txt
+	< log.log grep -o -E "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > ip_list.txt
+	< log.log grep -o -P "[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > domains_list.txt
+	< log.log grep "error" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > error_list.txt
+	< log.log grep -o -E " HTTP/1.(0|1)\" [0-9]{3,3} " | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > http_list.txt
 	dateTimeOld="Первый запуск"
 else
-	cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep -o -E "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > ip_list.txt
-	cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep -o -P "[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > domains_list.txt
-	cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep "error" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > error_list.txt
-	cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep -o -E " HTTP/1.(0|1)\" [0-9]{3,3} " | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > http_list.txt
-	dateTimeOld=`cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep  '______' | cut -f 2 -d '*'`
+	< log.log awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep -o -E "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > ip_list.txt
+	< log.log awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep -o -P "[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > domains_list.txt
+	< log.log awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep "error" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > error_list.txt
+	< log.log awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep -o -E " HTTP/1.(0|1)\" [0-9]{3,3} " | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > http_list.txt
+	dateTimeOld=$(< log.log awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep  '______' | cut -f 2 -d '*')
 	sed -i -e "s/___Start_Script_Send_Mail___/___OFF_Script_Send_Mail___/g" log.log
 fi
 
-echo ___Start_Script_Send_Mail___ >> log.log
+echo "___Start_Script_Send_Mail___" >> log.log
 
 dateTimeIn=$(date)
-echo ______*${dateTimeIn}*______ >> log.log
+echo "______*${dateTimeIn}*______" >> log.log
 
-ipMax=`awk '{if(max<$1){max=$1;line=$1}}END{print line}' ip_list.txt`
-cat ip_list.txt | grep "^${ipMax} " > ip_max.txt
+ipMax=$(awk '{if(max<$1){max=$1;line=$1}}END{print line}' ip_list.txt)
+< ip_list.txt grep "^${ipMax} " > ip_max.txt
 
-domainsMax=`awk '{if(max<$1){max=$1;line=$1}}END{print line}' domains_list.txt`
-cat domains_list.txt | grep "^${domainsMax} " > domains_max.txt
+domainsMax=$(awk '{if(max<$1){max=$1;line=$1}}END{print line}' domains_list.txt)
+< domains_list.txt grep "^${domainsMax} " > domains_max.txt
 
-ipListMax=`cat ip_max.txt`
-domainsListMax=`cat domains_max.txt`
-errorList=`cat error_list.txt`
-httpList=`cat http_list.txt`
+ipListMax=$(cat ip_max.txt)
+domainsListMax=$(cat domains_max.txt)
+errorList=$(cat error_list.txt)
+httpList=$(cat http_list.txt)
 
 echo "Диапазон времени:\n${dateTimeOld} - ${dateTimeIn}\n${ipListMax}\n${domainsListMax}\n${errorList}\n${httpList}" | mail -s "Отчёт по лог файлу" andrey@mail.ru
 
