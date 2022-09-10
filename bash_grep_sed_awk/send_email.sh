1023448f16e0a681b5b604ae7757ba9027816933
andrey21x6 <91377497+andrey21x6@users.noreply.github.com>
2022-09-10 08:59:33 +0500

    Update send_email.sh

--- bash_grep_sed_awk/send_email.sh
+++ bash_grep_sed_awk/send_email.sh
@@ -17,20 +17,16 @@ if [ -z "${logTest}" ]; then
 	cat log.log | grep -o -P "[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > domains_list.txt
 	cat log.log | grep "error" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > error_list.txt
 	cat log.log | grep -o -E " HTTP/1.(0|1)\" [0-9]{3,3} " | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > http_list.txt
+	dateTimeOld="Первый запуск"
 else
 	cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep -o -E "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > ip_list.txt
 	cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep -o -P "[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > domains_list.txt
 	cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep "error" | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > error_list.txt
 	cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep -o -E " HTTP/1.(0|1)\" [0-9]{3,3} " | sort -n | uniq -c | awk '{gsub(/^[ \t]+| [ \t]+$/,""); print $0 }' | sort -n > http_list.txt
+	dateTimeOld=`cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep  '______' | cut -f 2 -d '*'`
+	sed -i -e "s/___Start_Script_Send_Mail___/___OFF_Script_Send_Mail___/g" log.log
 fi
 
-dateTimeOld=`cat log.log | awk 'go { print } $0 == "___Start_Script_Send_Mail___" { go = 1 }' | grep  '______' | cut -f 2 -d '*'`
-
-if [ -z "${dateTimeOld}" ]; then
-	dateTimeOld="Первый запуск"
-fi
-
-sed -i -e "s/___Start_Script_Send_Mail___/___OFF_Script_Send_Mail___/g" log.log
 echo ___Start_Script_Send_Mail___ >> log.log
 
 dateTimeIn=$(date)
