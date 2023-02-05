4925b2fccabfce18ead551db9602c581eddf647c
andrey21x6 <andrey21x6@gmail.com>
2023-02-05 14:30:16 +0500

    2023-02-05 14:30:16

--- project1/bash/logserver.sh
+++ project1/bash/logserver.sh
@@ -86,3 +86,78 @@ sed -i -e "s/\#server.host: \"localhost\"   /   server.host: \"192.168.90.17\"/g
 
 # Смена пароля
 #/usr/share/elasticsearch/bin/elasticsearch-reset-password -i -u kibana_system
+
+#========================================================================================================================
+
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+
+
+
+nano /etc/yum.repos.d/logstash.repo
+
+[logstash-8.x]
+name=Elastic repository for 8.x packages
+baseurl=https://artifacts.elastic.co/packages/8.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+
+
+
+dnf install logstash -y
+
+systemctl enable logstash.service
+
+
+
+#nano /etc/logstash/logstash.yml - не трогаем
+
+
+
+nano /etc/logstash/conf.d/input.conf
+
+input {
+  beats {
+    port => 5044
+  }
+}
+
+
+
+nano /etc/logstash/conf.d/output.conf
+
+output {
+        elasticsearch {
+            hosts    => "https://127.0.0.1:9200"
+            index    => "websrv-%{+YYYY.MM}"
+	    user => "elastic"
+	    password => "123456"
+	    cacert => "/etc/logstash/certs/http_ca.crt"
+        }
+}
+
+
+
+nano /etc/logstash/conf.d/filter.conf
+
+filter {
+
+ if [type] == "nginx_access" {
+ 
+    grok {
+        match => { "message" => "%{IPORHOST:remote_ip} - %{DATA:user} \[%{HTTPDATE:access_time}\] \"%{WORD:http_method} %{DATA:url} HTTP/%{NUMBER:http_version}\" %{NUMBER:response_code} %{NUMBER:body_sent_bytes} \"%{DATA:referrer}\" \"%{DATA:agent}\"" }
+    }
+  }
+  
+  date {
+        match => [ "timestamp" , "dd/MMM/YYYY:HH:mm:ss Z" ]
+  }
+}
+
+
+
+systemctl start logstash.service
+#systemctl status logstash.service
+
