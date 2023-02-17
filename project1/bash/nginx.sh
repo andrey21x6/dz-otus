391dac54494fdd15822a419af537762ade36e4aa
andrey21x6 <andrey21x6@gmail.com>
2023-02-17 12:27:34 +0500

    2023-02-17 12:27:34

--- project1/bash/nginx.sh
+++ project1/bash/nginx.sh
@@ -11,6 +11,21 @@ echo ""
 dnf install openssl -y
 
 echo ""
+echo " *** Установка iptables-services ***"
+echo ""
+dnf install iptables-services -y
+
+echo ""
+echo " *** Включаем автозапуск iptables ***"
+echo ""
+systemctl enable iptables
+
+echo ""
+echo " *** Старт iptables ***"
+echo ""
+systemctl start iptables
+
+echo ""
 echo " *** Разрешение в SELinux на обратное проксирование ***"
 echo ""
 setsebool -P httpd_can_network_connect 1
@@ -66,3 +81,29 @@ echo ""
 echo " *** Старт nginx ***"
 echo ""
 systemctl start nginx
+
+echo ""
+echo " *** Установка iptables-services ***"
+echo ""
+dnf remove nftables -y
+dnf install iptables-services -y
+systemctl enable iptables
+systemctl start iptables
+
+echo ""
+echo " *** Настройка IPTABLES ***"
+echo ""
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables-save > fwoff
+
+echo ""
+echo " *** Добавляется задание в cron, при старте восстанавливает правила IPTABLES ***"
+echo ""
+echo "@reboot root iptables-restore < fwoff" >> /etc/crontab
