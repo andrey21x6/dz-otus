a3a9592467f6c8e9fc08a7234027bf64724c4be6
andrey21x6 <andrey21x6@gmail.com>
2023-02-18 19:09:56 +0500

    2023-02-18 19:09:56

--- MySQL_backup_replica/bash/backup_sql.sh
+++ MySQL_backup_replica/bash/backup_sql.sh
@@ -2,14 +2,14 @@
 
 FOLDERBACKUP=/home/vagrant/BACKUP/SQL
 FOLDERBACKUP2=/home/vagrant/BACKUP/mariabackup
-#FILE=SQL_2_Backup_$(date +"%d")
-FILE=SQL_2_Backup_$(date "+%d-%m-%Y-%H-%M-%S")
-FILE2=SQL_mariabackup_2_$(date "+%d-%m-%Y-%H-%M-%S")
+#FILE=SQL_Backup_$(date +"%d")
+FILE=SQL_Backup_$(date "+%d-%m-%Y-%H-%M-%S")
+FILE2=SQL_mariabackup_$(date "+%d-%m-%Y-%H-%M-%S")
 
 rm -rf $FOLDERBACKUP/*
 rm -rf $FOLDERBACKUP2/*
 
-mysqldump --single-transaction bet_odds > $FOLDERBACKUP/$FILE.sql
+mysqldump --single-transaction project1 text_entries > $FOLDERBACKUP/$FILE.sql
 mariabackup -u mariabackup --backup --target-dir=$FOLDERBACKUP2
 
 tar -cvf $FOLDERBACKUP/$FILE.tar.gz -P $FOLDERBACKUP/$FILE.sql
