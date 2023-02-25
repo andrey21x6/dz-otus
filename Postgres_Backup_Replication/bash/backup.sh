8d57d94d81bdfd9cc7eb1a3fa2694ae4ff0cd428
andrey21x6 <andrey21x6@gmail.com>
2023-02-25 12:04:27 +0500

    2023-02-25 12:04:27

--- Postgres_Backup_Replication/bash/backup.sh
+++ Postgres_Backup_Replication/bash/backup.sh
@@ -3,6 +3,7 @@
 nameDb=demodb
 FOLDERBACKUP=/var/lib/pgsql/backups
 FILE=backup_${nameDb}_$(date "+%d-%m-%Y-%H-%M-%S")
+FOLDERBACKUP_PG=/var/lib/pgsql/backups/${nameDb}_$(date "+%d-%m-%Y-%H-%M-%S")
 
 su - postgres -c "pg_dump ${nameDb} > backups/${FILE}.dump"
 
@@ -10,4 +11,6 @@ tar -cvf $FOLDERBACKUP/$FILE.tar.gz -P $FOLDERBACKUP/$FILE.dump
 
 rm -f $FOLDERBACKUP/$FILE.dump
 
-su - postgres -c "pg_basebackup -D archive_b/ -Ft -z"
+mkdir $FOLDERBACKUP_PG
+chown -R postgres:postgres $FOLDERBACKUP_PG
+su - postgres -c "pg_basebackup -D ${FOLDERBACKUP_PG}/ -Ft -z"
